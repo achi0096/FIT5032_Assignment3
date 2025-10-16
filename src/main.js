dffc35be9eb6bc6a0484da8161fc9608d7a06720
abinaya <achi0096@student.monash.edu>
2025-10-16 22:08:55 +1100

    Assignment3 initial set up

--- src/main.js
+++ src/main.js
@@ -6,6 +6,24 @@ import App from './App.vue'
 import router from './router'  
 
 
-const app = createApp(App)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChV-6qoF01t48ZXKNi_irW3KD9bMgowSY",
+  authDomain: "assignment3-66074.firebaseapp.com",
+  projectId: "assignment3-66074",
+  storageBucket: "assignment3-66074.firebasestorage.app",
+  messagingSenderId: "849807210801",
+  appId: "1:849807210801:web:1acf70e873122515249810"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// const app = createApp(App)
 app.use(router)                                 
 app.mount('#app')
