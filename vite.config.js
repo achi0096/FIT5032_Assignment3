3362bd8de09b30cd914f41c714de3412807c87ee
abinaya <achi0096@student.monash.edu>
2025-11-06 05:13:49 +1100

    update

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
-import path from 'path' 
+import path from 'path'
+
+const disablePWA = process.env.CI === 'true' || process.env.CF_PAGES === '1' 
 
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
+      disable: disablePWA, 
       workbox: {
-        maximumFileSizeToCacheInBytes: 7 * 1024 * 1024, 
-      },      
+        maximumFileSizeToCacheInBytes: 8 * 1024 * 1024,
+      },
     }),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), 
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 6000,
-  },
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  build: { chunkSizeWarningLimit: 6000 },
 })
